f4effb372231ceb2bb5bff1e5e7e4ced0d8793b3
pelzmagic <akinloyepelz@gmail.com>
2024-12-18 16:36:54 +0100

    add animation to style

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("how are you");
+const arrowButton = document.querySelector(".arrow");
+const buttonText = document.querySelector(".more");
+const overlay = document.querySelector(".overlay");
+const quoteSection = document.querySelector(".quote-wrapper");
+const clock = document.querySelector(".clock_details_wrapper");
+const arrow = document.querySelector(".arrow_image");
+
+arrowButton.addEventListener("click", () => {
+  console.log("how are you?");
+  quoteSection.style.display = "none";
+  clock.style.transform = "translateY(-120%)";
+  overlay.style.opacity = "1";
+  arrow.style.transform = "rotate(180deg)";
+});
